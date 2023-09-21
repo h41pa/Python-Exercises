d0d7d5cd11a4682429c46169d7374ae77f2f9f28
h41pa <madalin_chelu@yahoo.com>
2023-09-21 14:23:02 +0300

    update

---
+++ oopadvanced.py
@@ -0,0 +1,141 @@
+"""
+OOP - advance
+"""
+"""
+ABSTRACTION 
+Clasa abstractă FormaGeometrica
+Conține un field PI=3.14
+Conține o metodă abstractă aria (opțional)
+Conține o metodă a clasei descrie() - aceasta printează pe ecran ‘Cel mai probabil am colturi’
+
+"""
+
+# from abc import ABC, abstractmethod
+#
+#
+# class FormaGeometrica(ABC):
+#     PI = 3.14
+#
+#     @abstractmethod
+#     def aria(self):
+#        return NotImplementedError
+
+    # @abstractmethod    # obligatoriu de implementant
+    # def descrie(self):
+    #     print("Cel mai probabil am colțuri")
+
+    # @classmethod  # se poate apela din subclase cls face referire la clasa.
+    # def describe(cls):
+    #     print("Cel mai probabil am colțuri")
+
+"""
+INHERITANCE
+Clasa Pătrat - moștenește FormaGeometrica
+constructor pentru latură
+
+"""
+
+
+# class Patrat(FormaGeometrica):
+#     def __init__(self, latura):
+#         self.latura = latura
+
+"""
+ENCAPSULATION
+latura este proprietate privată
+Implementează getter, setter, deleter pentru latură
+Implementează metoda cerută de interfață (opțional, doar dacă ai ales să implementezi metoda abstractă aria)
+
+
+"""
+# from abc import ABC, abstractmethod
+#
+#
+# class FormaGeometrica(ABC):
+#     PI = 3.14
+#
+#     @abstractmethod
+#     def aria(self):
+#        return NotImplementedError
+#
+#
+#     @classmethod  # se poate apela din subclase cls face referire la clasa.
+#     def describe(cls):
+#         print("Cel mai probabil am colțuri")
+#
+#
+# class Patrat(FormaGeometrica):
+#     def __init__(self, latura):
+#         self.__latura = latura
+#
+#
+#     @property
+#     def latura(self):
+#         return self.__latura
+#
+#     @latura.setter
+#     def latura(self, noua_latura):
+#         self.__latura = noua_latura
+#
+#     @latura.deleter
+#     def latura(self):
+#         print("Ștergerea laturii nu este permisă.")
+#
+#     def aria(self):
+#         return self.__latura * 2
+#
+# patrat = Patrat(5)
+# print(patrat.latura)
+# print(patrat.aria())
+
+"""
+Clasa Cerc - moștenește FormaGeometrica
+constructor pentru rază
+raza este proprietate privată
+Implementează getter, setter, deleter pentru rază
+Implementează metoda cerută de interfață - în calcul folosește field PI mostenit din clasa părinte (opțional,
+doar dacă ai ales să implementezi metoda abstractă aria)
+
+
+"""
+
+from abc import ABC, abstractmethod
+
+
+class FormaGeometrica(ABC):
+    PI = 3.14
+
+    @abstractmethod
+    def aria(self):
+       return NotImplementedError
+
+
+    @classmethod
+    def describe(cls):
+        print("Cel mai probabil am colțuri")
+
+
+
+class Cerc(FormaGeometrica):
+    def __init__(self, raza):
+        self.__raza = raza
+
+    @property
+    def raza(self):
+        return self.__raza
+    @raza.setter
+    def raza(self, noua_raza):
+        self.__raza =  noua_raza
+    @raza.deleter
+    def raza(self):
+        print('Nu poti sterge raza')
+
+    def aria(self):
+        return self.PI * (self.raza ** 2)
+
+cerc1 = Cerc(4)
+print(f'Raza cercului este de {cerc1.raza}')
+print(f'Aria Cercului este {cerc1.aria()}')
+cerc1.raza = 7
+print(f'Noua raza cercului este de {cerc1.raza}')
+print(f'Noua arie a cercului este {cerc1.aria()}')
--- oopclasesiobiecte.py
+++ oopclasesiobiecte.py
@@ -182,110 +182,49 @@ schimbă_nume_produs(nume)
 generează_factura() - va printa tabelar dacă reușiți
 
 """
-# import datetime
-# class Factura:
-#     SERIE = 'A0001'
 #
-#     def __init__(self, numar: object, nume_produs: object, cantitate: object, pret_buc: object) -> object:
+# from datetime import date
+#
+# class Factura:
+#     # Atribut constant pentru serie
+#     serie = "x"
+#
+#     # Constructor
+#     def __init__(self, numar, nume_produs, cantitate, pret_buc):
 #         self.numar = numar
 #         self.nume_produs = nume_produs
 #         self.cantitate = cantitate
 #         self.pret_buc = pret_buc
+#         self.data = date.today()
 #
-#     def schimba_cantitate(self, cantitate):
+#     # Metoda pentru a schimba cantitatea
+#     def schimba_cantitatea(self, cantitate):
 #         self.cantitate = cantitate
+#
+#     # Metoda pentru a schimba prețul
 #     def schimba_pretul(self, pret):
 #         self.pret_buc = pret
+#
+#     # Metoda pentru a schimba numele produsului
 #     def schimba_nume_produs(self, nume):
 #         self.nume_produs = nume
 #
+#     # Metoda pentru a genera factura
 #     def genereaza_factura(self):
-#         total =  self.cantitate * self.pret_buc
-#         date_now = datetime.date.today()
-#         print(f'Factura seria {self.SERIE} numar {self.numar}\n Data: {date_now}')
-#         print(f'Produs\t|Cantitate\t|Pret Bucata\t|Total')
-#         print(f'{self.nume_produs}\t|{self.cantitate}\t|{self.pret_buc}\t|{total}')
+#         total = self.cantitate * self.pret_buc
+#         print(f"Factura seria {self.serie} număr {self.numar}")
+#         print(f"Data: {self.data.strftime('%d/%m/%Y')}")
+#         print("Produs | Cantitate | Preț bucată | Total")
+#         print(f"{self.nume_produs} | {self.cantitate} | {self.pret_buc} | {total}")
 #
-# telefon = Factura('12','Telefon', 2, 500)
-# telefon.schimba_cantitate(4)
-# telefon.schimba_pretul(800)
-# telefon.genereaza_factura()
-from datetime import datetime
-
-# print(datetime.today())
-
-
-class Factura:
-    serie = 'A0001'
-
-    def __init__(self, numar):
-        self.numar = numar
-        self.date = None
-        self.total_produs = None
-        self.pret_buc = None
-        self.cantitate = None
-        self.lista_produse = {}
-        self.nume_produs = None
-        self.total_plata = None
-
-    def adauga_produs(self, nume_produs, cantitate, pret_buc):
-        self.lista_produse[nume_produs] = list()
-        self.lista_produse[nume_produs].append(cantitate)
-        self.lista_produse[nume_produs].append(pret_buc)
-        self.lista_produse[nume_produs].append(self.get_total_produs(nume_produs))
-
-    def get_total_produs(self, nume_produs):
-        return self.lista_produse[nume_produs][0] * self.lista_produse[nume_produs][1]
-
-    def schimba_cantitatea(self, nume_produs, cantitate):
-        self.lista_produse[nume_produs][0] = cantitate
-        self.lista_produse[nume_produs][2] = self.get_total_produs(nume_produs)
-
-    def schimba_pretul(self, nume_produs, pret):
-        self.lista_produse[nume_produs][1] = pret
-        self.lista_produse[nume_produs][2] = self.get_total_produs(nume_produs)
-
-    def sterge_produsul(self, nume_produs):
-        del self.lista_produse[nume_produs]
-
-    def schimba_numele_produsului(self, nume_produs, nume_nou):
-        self.lista_produse[nume_nou] = self.lista_produse[nume_produs]
-        self.sterge_produsul(nume_produs)
-
-    def get_date(self):
-        self.date = datetime.today()
-        return self.date
-
-    def genereaza_factura(self):
-        self.total_plata = 0
-        print(f"Factura seria {self.serie} numar {self.numar}")
-        print(f"Data: {self.get_date()}")
-        col1, col2, col3, col4 = "Produs", 'Cantitate', 'Pret bucata', 'Total produs'
-        col_size = 20
-
-        print(f"{col1:^{col_size}}|{col2:^{col_size}}|{col3:^{col_size}}|{col4:^{col_size}}|")
-        for produs in self.lista_produse.keys():
-            print(f"{str(produs):^{col_size}}", end="|")
-            for coloana in self.lista_produse[produs]:
-                print(f"{str(coloana):^{col_size}}", end="|")
-            print()
-        for produs in self.lista_produse.keys():
-            self.total_plata += self.lista_produse[produs][2]
-        print('-' * 4 * col_size)
-        print(f"Total de plata: {self.total_plata}")
-        print('-' * 4 * col_size)
+# # Exemplu de utilizare
+# factura1 = Factura(1, "Telefon", 7, 700)
+# factura1.genereaza_factura()
+# factura1.schimba_cantitatea(10)
+# factura1.schimba_pretul(750)
+# factura1.schimba_nume_produs("Telefon Samsung")
+# factura1.genereaza_factura()
+#
+#
 
 
-f1 = Factura(1)
-f1.adauga_produs('Telefon', 7, 700)
-f1.adauga_produs('TV', 1, 2000)
-f1.adauga_produs('Frigider', 2, 1500)
-f1.genereaza_factura()
-f1.schimba_cantitatea('TV', 5)
-f1.genereaza_factura()
-f1.schimba_pretul('Telefon', 1000)
-f1.genereaza_factura()
-f1.schimba_numele_produsului('Telefon', 'Congelator')
-f1.genereaza_factura()
-f1.sterge_produsul('TV')
-f1.genereaza_factura()
