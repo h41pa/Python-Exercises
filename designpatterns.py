e8b7643341d4dd8b9a5658a19f3120c89a926181
h41pa <madalin_chelu@yahoo.com>
2023-09-27 11:16:55 +0300

    new exercise

--- designpatterns.py
+++ designpatterns.py
@@ -120,3 +120,83 @@ print(f'In spaniola zicem {spanish_trans.localize("masina")}')
 # # english_trans.localize('masina')
 # # french_trans = factory.get_language('fr')
 # # french_trans.localize('masina')
+
+#    ``` alta modalitate cu clasa abstracta
+
+# from abc import ABC
+#
+# class AbstractTranslator(ABC):
+#
+#     def localize(self, text):
+#         return NotImplementedError
+#
+#
+# class EnglishTranslator(AbstractTranslator):
+#
+#     def __init__(self):
+#         self.translations = {
+#             'masina': 'car',
+#             'om': 'human',
+#             'curs': 'course',
+#             'salut!': 'hello!'
+#         }
+#
+#     def localize(self, text):
+#         if text in self.translations:
+#             return self.translations[text]
+#         print('Traducerea nu exista')
+#
+# class SpanishTranslator(AbstractTranslator):
+#
+#     def __init__(self):
+#         self.translations = {
+#             'masina': 'coche',
+#             'om': 'hombre',
+#             'curs': 'clase',
+#             'salut!': 'hola!'
+#         }
+#
+#
+#     def localize(self, text):
+#         if text in self.translations:
+#             return self.translations[text]
+#         print('Traducerea nu exista...')
+#
+# class FrenchTranslator(AbstractTranslator):
+#     def __init__(self):
+#         self.translations = {
+#             'masina': 'voiture',
+#             'om': 'homme',
+#             'curs': 'course',
+#             'salut!': 'bonjour!'
+#         }
+#     def localize(self, text):
+#         if text in self.translations:
+#             return self.translations[text]
+#
+#         print('Traducerea nu exista...')
+#
+#
+# class TranslatorFactory:
+#     def __init__(self):
+#         pass
+#
+#     @staticmethod
+#     def get_translator(language):
+#         if language == 'en':
+#             return EnglishTranslator()
+#         elif language == 'fr':
+#             return FrenchTranslator()
+#         elif language == 'es':
+#             return SpanishTranslator()
+#         else:
+#             return ValueError(f'limba nu exista {language}')
+#
+#
+# translators = []
+# translators.append((TranslatorFactory.get_translator('en')))
+# translators.append((TranslatorFactory.get_translator('fr')))
+# translators.append((TranslatorFactory.get_translator('es')))
+#
+# for t in translators:
+#     print(t.localize('masina'))
